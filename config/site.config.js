65ede9762f109b5c756151e374408c45cce81e71
dariNirvana <78086755+Ziang-Liu@users.noreply.github.com>
2024-10-19 19:42:14 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "dariNirvana's Warehouse",
+  title: "dari的仓库",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
@@ -53,7 +53,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/Ziang-Liu/CATS-NetDisk',
+      link: 'https://github.com/Ziang-Liu/OD-Vercel-Index-Fork',
     },
   ],
 
